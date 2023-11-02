6db8f5e19e20553a50d236921e86754444c93fa9
Flukeznpm <nattapong.meun@gmail.com>
2023-11-03 02:26:45 +0700

    [ADD] Kafka basic producer to consumer

--- consumer.js
+++ consumer.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
 
-const axios = require('axios')
-const { Kafka } = require('kafkajs')
-const { Order } = require('./schema')
+// const axios = require('axios')
+// const { Kafka } = require('kafkajs')
+// const { Order } = require('./schema')
 const kafka = require('./kafka')
 
-const LINE_API_PUSH_MESSAGE_URL = 'https://api.line.me/v2/bot/message/push'
-const LINE_ACCESS_TOKEN = process.env.LINE_ACCESS_TOKEN
+// const LINE_API_PUSH_MESSAGE_URL = 'https://api.line.me/v2/bot/message/push'
+// const LINE_ACCESS_TOKEN = process.env.LINE_ACCESS_TOKEN
 
 const consumer = kafka.consumer({ groupId: 'message-group' })
 
